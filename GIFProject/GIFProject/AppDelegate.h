e20f3c3e698afcf61f48a8ba94f8e135cc95bc45
daling <sun409377708@163.com>
2016-10-14 10:09:07 +0800

    create project

---
+++ GIFProject/GIFProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIFProject
+//
+//  Created by maoge on 16/10/14.
+//  Copyright © 2016年 maoge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
